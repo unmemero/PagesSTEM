e24237bfacfbe3cb21d134c89aa7aa9395d13f5f
Rafael Garcia <fafagm99@gmail.com>
2024-08-22 10:41:05 -0600

    Added frontend and admin folders

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 function auth(req, res, next){
-    const tokeen = req.headr('x-auth-token');
+    const token = req.header('x-auth-token');
     if(!token){
         return res.status(401).json({message:'No token, auth denied'});
     }
